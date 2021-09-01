2a693db6659fb77afa165f2c0a4a99cbe0b2548f
Gianmarco <gianmarcocasati1@gmail.com>
2021-08-31 21:39:58 -0300

    Create user doc

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express, { NextFunction, Request, Response } from "express";
+import swaggerUi from "swagger-ui-express";
 
 import AppError from "./errors/AppError";
 import { usersRoutes } from "./routes/users.routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
-
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/users", usersRoutes);
 app.use(
   (error: Error, request: Request, response: Response, _next: NextFunction) => {
